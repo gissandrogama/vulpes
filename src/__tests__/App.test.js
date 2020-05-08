a164fc67bed7dd933fa89181e16876af54ae1bcf
gissandrogama <gissandrogama@gmail.com>
2020-05-08 16:50:30 -0300

    create project and test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+// import renderer from 'react-test-renderer';
 import App from '../App'
 
 describe('Testing App component', () => {
   it('should render correctly', () => {
-    const wrapper = shallow(
-      <App />
-    )
+    const wrapper = shallow(<App />)
 
-    expect(wrapper).toMatchSnapshot()
-  })
-})
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
