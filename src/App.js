2dccdcd1facf6822d4f007615adb2451a9980f3b
gissandrogama <gissandrogama@gmail.com>
2020-05-21 00:28:23 -0300

    menu refeito e reorganizando o home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Routes from './routes'
 
-import Header from './components/Header/Header'
+import logo from './assets/logo3.png'
 
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header />       
-      <Routes />
+    <nav className="menu">
+      <div className="logo">
+          <img src={logo} alt="Vulpes"/>          
+      </div>
+
+      <ul className="container">
+        <li><a className="active" href="/">HOME</a></li>
+        <li><a href="/loja">LOJA</a></li>
+        <li><a href="#">EQUIPES</a></li>
+        <li><a href="#">PARCEIROS</a></li>
+        <li><a href="/sobre">SOBRE</a></li>
+      </ul>
+    </nav>      
+    <Routes />
     </>
   );
 }
